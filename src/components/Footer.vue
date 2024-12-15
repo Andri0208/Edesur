8f6c2a246d5059b2831cf78a8c7bab4df039afea
andri <delacruzandri8@gmail.com>
2024-12-15 16:08:10 -0400

    agregar variables

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,104 +1,103 @@
 <script setup>
-
 const img = {
-    img1: 'public/logo.png.png',
+  img1: 'public/logo.png.png',
 }
-
 </script>
 
 <template>
-<div class="container footer ">
-  <div class="row align-items-start">
-    <div class="col-xl-2 col-md-12 img">
+  <div class="container footer">
+    <div class="row align-items-start">
+      <!-- Columna para la imagen -->
+      <div class="col-xl-2 col-md-12 img">
         <img :src="img.img1" class="img-fluid" alt="imagen de edesur dominicana">
-    </div>
-    <div class="col">
+      </div>
+
+      <!-- Columna con los diferentes bloques de información -->
+      <div class="col">
         <div class="row">
-            <div class="col-xl-3">
-      <ul>
-        <h5>CONÓCENOS</h5>
-      </ul>
-      <ul>
-        <a class="nav-link" href="#">
-            <p>Edesur Dominicana S.A</p>
-            </a>
-      </ul>
-      </div>
-      <div class="col-xl-3">
-        <ul>
-            <h5>CONTÁCTANOS</h5>
-        </ul>
-        <ul>
-            <li>Tel: (809) 683-9292</li>
-        </ul>
-        <ul>
-            <li>Fax: (809)-683-9393</li>
-        </ul>
-        <ul>
-            <li>info@edesur.com.do</li>
-        </ul>
-      </div>
-      <div class="col-xl-3">
-        <ul>
-            <h5>BÚSCANOS</h5>
-        </ul>
-        <ul>
-            <p>Av. Tiradentes #47 Esq. Carlos Sánchez y
-Sánchez, Torre Serrano, Ensanche Naco, Distrito
-Nacional, República Dom.</p>
+          <!-- Conócenos -->
+          <div class="col-xl-3">
+            <ul>
+              <h5>CONÓCENOS</h5>
+              <li>
+                <a class="nav-link" href="#">
+                  <p>Edesur Dominicana S.A</p>
+                </a>
+              </li>
             </ul>
+          </div>
 
-      </div>
-      <div class="col-xl-3">
-        <ul>
-            <h5>INFÓRMATE</h5>
-        </ul>
-        <ul>
-            <a class="nav-link" href="#">
-            <p>Términos de Uso</p>
-            </a>
-        </ul>
-        <ul>
-            <a class="nav-link" href="#">
-            <p>Políticas de Privacidad</p>
-            </a>
-        </ul>
-        <ul>
-            <a class="nav-link" href="#">
-            <p>Preguntas Frecuentes</p>
-            </a>
-        </ul>
-      </div>
+          <!-- Contáctanos -->
+          <div class="col-xl-3">
+            <ul>
+              <h5>CONTÁCTANOS</h5>
+              <li>Tel: (809) 683-9292</li>
+              <li>Fax: (809)-683-9393</li>
+              <li>info@edesur.com.do</li>
+            </ul>
+          </div>
+
+          <!-- Búscanos -->
+          <div class="col-xl-3">
+            <ul>
+              <h5>BÚSCANOS</h5>
+              <li>
+                <p>Av. Tiradentes #47 Esq. Carlos Sánchez y Sánchez, Torre Serrano, Ensanche Naco, Distrito Nacional, República Dominicana.</p>
+              </li>
+            </ul>
+          </div>
+
+          <!-- Infórmate -->
+          <div class="col-xl-3">
+            <ul>
+              <h5>INFÓRMATE</h5>
+              <li>
+                <a class="nav-link" href="#">
+                  <p>Términos de Uso</p>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">
+                  <p>Políticas de Privacidad</p>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">
+                  <p>Preguntas Frecuentes</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
-  
-</div>
-<footer class="">
+
+  <!-- Footer -->
+  <footer>
     <div class="container d-flex justify-content-between align-items-center contenedor">
-    <div>
-    <p>© 2023 Todos los Derechos Reservados. Desarrollado por <span>Edesur Dominicana S.A</span></p>
-    </div>
-    <div class="d-flex align-items-center ">
-        <p>SIGUENOS</p>
+      <div>
+        <p>© 2023 Todos los Derechos Reservados. Desarrollado por <span>Edesur Dominicana S.A</span></p>
+      </div>
+      <div class="d-flex align-items-center">
+        <p>SÍGUENOS</p>
         <a href="#">
-        <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-facebook"></i>
         </a>
         <a href="#">
-            <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-twitter"></i>
         </a>
         <a href="#">
-            <i class="fa-brands fa-instagram"></i> 
+          <i class="fa-brands fa-instagram"></i>
         </a>
         <a href="#">
-            <i class="fa-brands fa-youtube"></i>
+          <i class="fa-brands fa-youtube"></i>
         </a>
-    </div>
+      </div>
     </div>
   </footer>
 </template>
-<style>
 
+<style>
 
 </style>
